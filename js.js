c9d48cee7f2bbeecb1bf3abad67ffbb8994bff3c
Rodrilundain <136036888+Rodrilundain@users.noreply.github.com>
2025-05-20 11:42:06 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 const form = new URLSearchParams(new FormData(document.getElementById('miFormulario')));
 
-fetch(scriptURL, {
+fetch(scriptURL, {https://script.google.com/u/0/home/projects/1tYJ9elFvWnE5_cVJ5qanrg-CLj6Ag27KS-dBapg2y2xXAVtSIj-u7zHI/edit
   method: 'POST',
   body: form,
   headers: {
